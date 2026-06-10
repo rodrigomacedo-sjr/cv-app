5b0fd624a579af7ddc291af5eebb83c9db3ba63a
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2026-06-10 11:32:35 -0300

    feat: pagination animation

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,31 @@ export function App() {
     <div className="flex flex-col flex-1 w-full">
       <div key={page} className="animate-page-enter flex flex-col flex-1">
         {page == 0 && <LandingPage handleNext={() => setPage(1)} />}
+
         {page == 1 && (
           <FormsPage
             handlePrev={() => setPage(0)}
-            handleNext={() => setPage(1)}
+            handleNext={() => setPage(2)}
           >
-            {" "}
+            {" General Information "}
+          </FormsPage>
+        )}
+
+        {page == 2 && (
+          <FormsPage
+            handlePrev={() => setPage(1)}
+            handleNext={() => setPage(3)}
+          >
+            {" Educational Experience "}
+          </FormsPage>
+        )}
+
+        {page == 3 && (
+          <FormsPage
+            handlePrev={() => setPage(2)}
+            handleNext={() => setPage(4)}
+          >
+            {" Professional Experience "}
           </FormsPage>
         )}
       </div>
